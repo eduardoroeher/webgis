c83993e3c4531751e4c2410393c1c8a615214910
eduardoroeher <35782747+eduardoroeher@users.noreply.github.com>
2024-08-19 21:29:55 -0300

    Add files via upload

---
+++ ola_mundo.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+    
+    // Exibe uma mensagem de Olá Mundo no console
+    console.log("Olá Mundo!");
+
+    // Exibe um alerta na página
+    alert("Olá Mundo! O script está funcionando corretamente.");
+
+})();
