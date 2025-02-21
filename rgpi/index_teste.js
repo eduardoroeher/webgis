829da9a70cdee3bf796247306b28760aabe5df6c
eduardoroeher <35782747+eduardoroeher@users.noreply.github.com>
2025-02-21 15:13:56 -0300

    Update index_teste.js

--- rgpi/index_teste.js
+++ rgpi/index_teste.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    console.log("O script já foi carregado anteriormente. - V_02");
+    console.log("O script já foi carregado anteriormente. - V_03");
 
     // Atalho "Alt + C" para formatação manual e clique no botão PDF
     document.addEventListener("keydown", function (event) {
